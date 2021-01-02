97fd507ff13b0c2ab0f1f43d8f0d9e001707fefd
Diego <diegog2021@gmail.com>
2021-01-03 00:05:27 +0100

    Add: Readme classes.

---
+++ clases/1.2_clase_intro_r.r
@@ -0,0 +1,20 @@
+#' ---
+#' title: "Clase 2"
+#' author: "Diego Gimenez"
+#' date: "13/01/2021"
+#' ---
+
+
+
+# Corramos nuestro primer comando!
+  # Hacer click en run o utilizar teclas ctrl-enter | cmd-enter.
+
+print("Hola Unimet!")
+
+
+# Esto es un comentario, no va a correr!
+  # Todos los comentarios en R son precedidos por un numeral (#)
+
+#Ejemplo: 
+  # El comando que utilizamos anteriormente no va a correr: 
+  # print("Hola Unimet!")
---
+++ clases/README.md
@@ -0,0 +1,13 @@
+# Información importante sobre `clases/`
+
+Acá van a poder conseguir todo el código de R que utilizaremos en clases.
+
+Cada script tiene el siguiente formato: ```<numero de semana>.<numero de clase de la semana>_<informacion sobre el script>```
+
+Ejemplo:
+
+- Script semana 3, primer clase de la semana.
+  - `3.1_tidy_data_intro.r`
+
+- Script semana 5, segunda clase de la semana.
+  - `5.2_diseno_experimental`
