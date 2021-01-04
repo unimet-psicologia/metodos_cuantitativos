b1963e06a7255cf84b72a49d555f70f36e08c1db
diego <diegog2021@gmail.com>
2021-01-04 06:43:59 -0500

    Add: Tidy data Intro.

---
+++ clases/3.1_tidy_data.r
@@ -0,0 +1,15 @@
+#' ---
+#' title: "Semana 3.1 - Tidy Data"
+#' author: "Diego Gimenez"
+#' date: "25/01/2021"
+#' ---
+
+
+## Informacion sobre el dataset:
+# https://catalog.data.gov/dataset/behavioral-risk-factor-data-tobacco-use-2011-to-present
+# https://bookdown.org/hneth/ds4psy/
+# Leyendo data en R -------------------------------------------------------
+
+# Instalar packete readr si no esta instalado
+
+library(readr)
