1c577a4f542f6da28d48e2a9b1c931e2d5041553
Diego <diegog2021@gmail.com>
2021-01-07 13:45:31 +0100

    Add: Univariate Descriptive.

--- clases/3.2_exploracion_data_decriptivos.r
+++ clases/3.2_exploracion_data_decriptivos.r
@@ -10,6 +10,9 @@ library(readr)
 library(haven)
 library(readxl)
 library(stringr)
+library(ggplot2)
+library(tidyr)
+library(psych)
 
 # Sobre el dataset:
 # https://beta.ukdataservice.ac.uk/datacatalogue/studies/study?id=854297
@@ -47,24 +50,133 @@ study_1_original_df %>% colnames()
 # Editar nombre de variables a un mejor formato ----------------------------------------
 
 # Es necesario que todas las columnas tengan el siguiente formato:
- # Esten en lower case
- # No tengan espacios
+# Esten en lower case
+# No tengan espacios
+
 
 # Esto va a hacer que nuestra vida sea Mucho mas sencilla!!
 
-colnames(study_1_original_df)<- study_1_original_df %>% 
+colnames(study_1_original_df) <- study_1_original_df %>%
   colnames() %>% # Seleccionamos el nombre de Todas columnas
-  tolower() %>%  # Lo convertimos a lower case
+  tolower() %>% # Lo convertimos a lower case
+
   str_replace(" ", "_") # Reemplazamos espacios con underscores
 
 colnames(study_1_original_df)
 
+
 # Seleccionemos las variables de interes ----------------------------------
 
-study_1_original_df %>% 
-  select(openness, conscientiousness, extraversion ,agreeableness, neuroticism, conservatism, media)
+# editemos el nombre de las variables sc0, sc1 y sc2 a algo mas entendible
+## sc0: Total Sharing
+# sc1: Total Truthful
+# sc2: Total Seen Before
+
+
+study_1_original_df <- study_1_original_df %>%
+  rename(total_sharing = sc0, total_truth = sc1, total_seen_before = sc2)
+
+
+# Ahora seleccionamos las variables de interes y creamos un nuevo dataframe
+
+study_1_subset_df <- study_1_original_df %>%
+  select(
+    age,
+    gender,
+    age_group,
+    country,
+    fbook,
+    occupation,
+    openness,
+    conscientiousness,
+    extraversion,
+    agreeableness,
+    neuroticism,
+    conservatism,
+    total_nmls,
+    total_sharing,
+    total_truth,
+    total_seen_before
+  )
+
+
+# Verifiquemos el tipo de data de cada variable ---------------------------
+
+study_1_subset_df %>%
+  str()
+
+study_1_subset_df %>%
+  select(gender)
+
+## Genero: Esta variable está codificada como numeric (double)
+
+# Ojo: Si no le decimos a R que esta variable es un factor, nos va a dar la
+# media de genero!
+
+study_1_subset_df$gender %>% summary()
+
+study_1_subset_df$gender %>% class()
+
+# Es por esto que tenemos que cambiar la variable a factor
+
+study_1_subset_df$gender <- study_1_subset_df$gender %>%
+  factor(
+    levels = c(1, 2, 3, 4),
+    labels = c("male", "female", "other", "pnts")
+  )
+
+# Mira la diferencia luego de haber creado el factor!
+study_1_subset_df$gender %>% summary()
+
+
+## Tu turno!
+# Que otras variables deberiamos cambiar de tipo?
+#
+#
+#
+
+# Veamos nuestros primeros datos descriptivos! ----------------------------
+
+
+study_1_subset_df %>%
+  summary()
 
 
 
+# Valores Nulos -----------------------------------------------------------
+
+# Total de valores nulos en el dataframe
+study_1_subset_df %>%
+  is.na() %>%
+  sum(axis = 1) # Axis = 1 Realiza la suma por columnas
+
+# Que sujetos tienen valores nulos?
+study_1_subset_df %>%
+  filter_all(any_vars(is.na(.)))
 
 
+# Que columnas tienen valores nulos?
+study_1_subset_df %>%
+  sapply(function(x) sum(is.na(x)))
+
+
+
+# Analisis descriptivo ----------------------------------------------------
+
+## Volvamos a utilizar summary
+
+study_1_subset_df %>% summary()
+
+## Tambien podemos utilizar describe del paquete psych
+describe(study_1_subset_df, mad = TRUE)
+
+# Hay un error en la funcion de arriba, cual crees que es?
+
+## Distribucion de grupos etarios
+
+study_1_subset_df %>% 
+  group_by(age_group) %>% 
+  summarise(frecuencia = n()) %>% # Contamos freqcuencia de cada grupo etario
+  mutate(porcentaje = frecuencia / sum(frecuencia) * 100) # Utilizamos frecuencia para calcular la proporcion
+
+# Visualizaciones ---------------------------------------------------------
