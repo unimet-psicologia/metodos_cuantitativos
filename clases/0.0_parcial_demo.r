e6098472b914927f4715beb0d7ee2dba339563ef
dieko95 <diegog2021@gmail.com>
2021-02-24 19:45:07 +0100

    Add: Demo parcial.

---
+++ clases/0.0_parcial_demo.r
@@ -0,0 +1,34 @@
+#' Nombre:
+#' Apellido:
+#' Cedula:
+#' Carnet de Estudiante:
+
+# Cargar paquetes --------------------------------------------------------
+
+
+# Leer data ---------------------------------------------------------------
+
+df <- ________
+
+# EDA Inicial -------------------------------------------------------------
+
+
+# EDA Univariante ---------------------------------------------------------
+
+
+# EDA Bivariante ----------------------------------------------------------
+
+
+# EDA Evaluacion de supuestos ---------------------------------------------
+# Factor: x
+# VD: variable_dependiente
+
+
+# ANOVA -------------------------------------------------------------------
+# Factor: x
+# VD: variable_dependiente
+
+
+# Chi Cuadrado ------------------------------------------------------------
+# la varaible x_1 y x_2 se son dependientes o independientes?
+# Calcula todos los estadisticos relevantes para reportar chi cuadrado
